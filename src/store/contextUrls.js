cc583b796a9191b24da73783c0853f1a646fbd85
Luca Orlandini <luca.orlandini@docomodigital.com>
2020-12-08 21:40:44 +0100

    rm default data

--- src/store/contextUrls.js
+++ src/store/contextUrls.js
@@ -1,13 +1,7 @@
 import React from "react";
-import { v4 as uuidv4 } from 'uuid';
 
 const Store = React.createContext({
     items: [
-        {
-            url: "pippo",
-            key: uuidv4(),
-            click: 0
-        }
     ]
 });
 
