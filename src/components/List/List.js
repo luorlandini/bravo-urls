02cd0af25038965644ad4b1070d5ed21b1389f60
Luca Orlandini <luca.orlandini@docomodigital.com>
2020-12-08 21:39:28 +0100

    add count click

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,10 +1,15 @@
 import React, { useContext } from "react";
-import { Button } from 'react-bootstrap';
+import { FaTrashAlt } from 'react-icons/fa';
+
+
 import Store from "./../../store";
 
 const List = () => {
 
     const { state, dispatch } = useContext(Store);
+    const handleUrlClick = (item) => {
+        dispatch({ type: "COUNT_VISIT", payload: { key: item.key, url: item.url, count: item.count + 1 } })
+    }
 
     return (
         <div className="row mt-5">
@@ -15,14 +20,17 @@ const List = () => {
                             {state.items.map(item => (
                                 <li key={item.key} className="list-group-item">
 
-                                    <Button href={item.url} variant="link">{item.url}</Button>
+                                    <a onClick={() => handleUrlClick(item)} href={item.url} rel="noreferrer" target="_blank" class="button">{item.url}</a>
                                     <button
                                         className="float-right btn btn-danger btn-sm"
                                         style={{ marginLeft: 10 }}
                                         onClick={() => dispatch({ type: "REMOVE", payload: item })}
+
                                     >
-                                        Remove
+                                        <FaTrashAlt />
                                     </button>
+                                    <span className="float-right" >Hit counter: {item.count} </span>
+
                                 </li>
                             ))}
                         </ul>
