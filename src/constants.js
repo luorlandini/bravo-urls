9a3aad4bdf7e619616ba9ea70612387d76c2183a
Luca Orlandini <luca.orlandini@docomodigital.com>
2020-12-09 16:23:14 +0100

    END POINT CONST

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const DOMAIN_API = 'http://localhost:5000/';
+export const ENDPOINT_POST_URLS = 'api/urls';
+export const ENDPOINT_KEY = 'api/:key';
+export const ENDPOINT_DELETE = '/api/urls/:key';
+export const ENDPOINT_KEY_STATS = '/api/urls/:key/stats';
